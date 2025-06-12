724429e942c8b3c8dee65453d268f76e4c51f6c8
soulmeant09@gmail.com <soulmeant09@gmail.com>
2025-06-12 17:58:18 +0900

    팀원 코드 병합 중 settings.py와 urls.py 충돌 해결 완료

--- flo_project/urls.py
+++ flo_project/urls.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.http import HttpResponse # 현재 home 뷰에서 사용 안 함
-from django.urls import include, path
-# from flo_project import settings # 아래 settings를 직접 사용하므로, 명시적으로 from django.conf import settings가 더 일반적
-from django.conf import settings # 이렇게 변경하는 것을 권장
-from django.conf.urls.static import static
-from django.shortcuts import render
-# from django.conf.urls.i18n import i18n_patterns # 현재 주석 처리됨
-
-# 이 home 뷰는 현재 urlpatterns에서 주석 처리되어 사용되지 않고 있습니다.
-# 만약 루트 URL을 flo 앱에서 처리한다면 이 함수는 필요 없을 수 있습니다.
-def home(request):
-    return render(request, 'flo/index.html')
-    
-urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-]
-
-urlpatterns += [
-     path('', include('flo.urls')),
-     path('admin/', admin.site.urls),
-]
-
-# 개발 환경에서 MEDIA 파일 서빙
-if settings.DEBUG: # settings.DEBUG 조건 추가 (권장)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# ... (주석 처리된 i18n_patterns) ...
-=======
 """
 URL configuration for flo_project project.
 
@@ -74,4 +44,3 @@ urlpatterns += [  # + 꼭 붙이기
 # 이미지는 web 경로를 쓰는데, 이를 허용하겠다.(요청 URL에 이미지를 요청하면 그 이미지를 보여주겠다.)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> b0590081b1e26a670b5c09461d4acb164dcc58f5
